8b7cbaddb2f5f1ee131c16b3a0da5a296a26d260
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-20 17:29:28 -0900

    Hello.cpp Finalization, Poem.cpp

--- HW0/Hello/Hello.cpp
+++ HW0/Hello/Hello.cpp
@@ -2,7 +2,7 @@
 * HW0
 *Hello.ccp
 * Clifton Terwilliger
-* date
+* 20 Jan 21
 * Hello World
 */
 
@@ -11,10 +11,12 @@ using namespace std;
 using std::cin;
 int main()
 {
+	//loop, prints Hello World 5 times//
 	for (int i = 0; i < 5; i++)
 	{
 		cout << "Hello World\n";
 	}
+	//exit code// 
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
 	return 0; 
--- HW0/Poem/Poem.cpp
+++ HW0/Poem/Poem.cpp
@@ -5,9 +5,14 @@
 * Name
 */
 
-#include <iostream>
 
+#include <iostream>
+using namespace std;
+using std::cin;
 int main()
 {
-
+	cout << "My little horse must think it queer\nTo stop without a farmhouse near\nBetween the woods and frozen lake\nThe darkest evening of the year.\n";
+	cout << "Press ENTER to quit ";
+	while (cin.get() != '\n');
+	return 0;
 }
