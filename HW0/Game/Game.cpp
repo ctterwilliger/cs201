fa4368b85a485de6f158a77c2b8b22821c79a051
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-20 17:40:21 -0900

    Poem Finalization and Game.cpp and its finalization.

--- HW0/Game/Game.cpp
+++ HW0/Game/Game.cpp
@@ -1,8 +1,8 @@
 /**
-*name.ccp
+* game.ccp
 * Clifton Terwilliger
-* date
-* Name
+* 20 Jan 21
+* Game
 */
 
 #include <iostream>
@@ -10,7 +10,10 @@ using namespace std;
 using std::cin;
 int main()
 {
-	cout << "My little horse must think it queer\nTo stop without a farmhouse near\nBetween the woods and frozen lake\nThe darkest evening of the year.\n";
+	//Prints Decription
+	cout << "Ark Survival Evolved\nDinasaur Survival Game, progress from bare fists, to tools more advanced than current humans.\n";
+	cout << "Has a fantastic yet not nessesary story\nYou can fight dinosaurs and even other players.\nYou can tame and ride dinasaurs, and harvest resources.\nUltimate Goal: Survive\n";
+	//Exit code
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
 	return 0;
--- HW0/Poem/Poem.cpp
+++ HW0/Poem/Poem.cpp
@@ -5,13 +5,15 @@
 * Name
 */
 
-
 #include <iostream>
 using namespace std;
 using std::cin;
 int main()
 {
+	//Prints poem exirpt
 	cout << "My little horse must think it queer\nTo stop without a farmhouse near\nBetween the woods and frozen lake\nThe darkest evening of the year.\n";
+	cout << "~Robert Frost\n";
+	//Exit code
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
 	return 0;
