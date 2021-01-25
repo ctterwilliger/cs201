2dd87ffcca625c3f10c3f8a7df82db964fe3e688
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-25 10:18:59 -0900

    lab_3-1 User input and User utilzation

--- LAB/Lab3-1/Lab3-1.cpp
+++ LAB/Lab3-1/Lab3-1.cpp
@@ -11,25 +11,55 @@ using namespace std;
 using std::cin;
 int main()
 {
-
-	//prints 1-20
+	// Collects Length
 	cout << "Program 1\n";
+	cout << "Please Enter a Positive Integer that you would like to print the numbers precedding it\n";
+	int print_length = 0;
+	cin >> print_length;
+	while (print_length <= 0)
+	{
+		cout << "Please Enter a Positive Integer\n";
+		cin >> print_length;
+	}
+
+	//prints 1 through User Entered Integer
 	int i = 1;
-	while (i <= 20) {
+	while (i <= print_length) {
 		cout << i <<" ";
 		i = i + 1;
 	}
 	cout << "\n"; 
-	cout << "Program 2\n";
-	//prints box
-	for (int i = 0; i < 10; i++)
+
+	cout << "Program 2 & 3\n";
+	//Collects Width and Hieght 
+	cout << "Please Enter the Width of the Box\n";
+	int box_w = 0;
+	cin >> box_w;
+	while (box_w <= 0)
 	{
-		for (int j = 0; j < 60; j++)
+		cout << "Please Enter a Positive Integer\n";
+		cin >> box_w;
+	}
+	cout << "Please Enter the Height of the Box\n";
+	int box_h = 0;
+	cin >> box_h;
+	while (box_h <= 0)
+	{
+		cout << "Please Enter a Positive Integer\n";
+		cin >> box_h;
+	}
+
+	//prints box
+	cout << "Program 2\n";
+	for (int i = 0; i < box_h; i++)
+	{
+		for (int j = 0; j < box_w; j++)
 		{
 			cout << "*";
 		}
 		cout << "\n";
 	}
+
 	cout << "Program 3\n";
 	//using modulo if to determine if the potition is even or odd to print alterating text
 	for (int i = 0; i < 10; i++)
