8e7c2ff84f8fa1e4d88b5a7ab525d8b38f1f972f
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-25 09:29:59 -0900

    lab3-1 addition

---
+++ LAB/Lab3-1/Lab3-1.cpp
@@ -0,0 +1,18 @@
+/**
+*Lab3-1
+*Lab3-1.ccp
+* Clifton Terwilliger
+* 25 Jan 21
+* loops
+*/
+
+#include <iostream>
+using namespace std;
+using std::cin;
+int main()
+{
+	//exit code// 
+	cout << "Press ENTER to quit ";
+	while (cin.get() != '\n');
+	return 0;
+}
