e8eacec2d00a8f3e5d9b8411295a222615241b91
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-29 13:48:34 -0900

    grid working

--- HW1/grid/grid.cpp
+++ HW1/grid/grid.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 using std::cin;
 
-int get_pos_int(string text)
+int get_positive_int(string text)
 {
 	cout << text; 
 	int get;
@@ -23,28 +23,69 @@ int get_pos_int(string text)
 	}
 	return get;
 }
+int get_position(string text, int max)
+{
+	int get; 
+	cout << text;
+	cin >> get;
+	while (get == 0)
+	{
+		cout << "Your number is 0, 0 is a valid position\n Please enter a positive number: ";
+		cin >> get;
+	}
+	while (get > max)
+	{
+		cout << "Your Number is too large\n Please enter a smaller number: ";
+		cin >> get;
+	}
+	return get; 
+}
 
 int main()
 {
 	int x = 1;
 	int y = 1;
 	cout << "This program will let you make a grid\n";
-	x = get_pos_int("Please enter the width of the grid: ");
-	y = get_pos_int("Please enter the hieght of the grid: ");
+	std::vector<std::vector<int>> grid;
+	x = get_positive_int("Please enter the width of the grid: ");
+	y = get_positive_int("Please enter the hieght of the grid: ");
+	grid.resize(x);
+	for (int i = 0; i < x; i++)
+	{
+		grid[i].resize(y);
+	}
+	bool end = false;
+	int x_pos = 0;
+	int y_pos = 0;
 	do
 	{
 		for (int i = 0; i < y; i++)
 		{
 			for (int j = 0; j < x; j++)
 			{
-				if (grid[j][i] ==)
-				cout << ".";
+				if (grid[j][i] == 1)
+				{
+					cout << "x";
+				}
+				else
+				{
+					cout << ".";
+				}
 			}
 			cout << "\n";
 		}
-
-	}while()
-	std::vector<std::vector<int>> grid;
+		y_pos = get_position("Please enter a row(negative number to end): ", y);
+		if (y_pos < 0)
+		{
+			break;
+		}
+		x_pos = get_position("Please enter a column(negative number to end): ", x);
+		if (x_pos < 0)
+		{
+			break;
+		}
+		grid[x_pos - 1][y_pos - 1] = 1;
+	}while(end == false);
 	//Exit code
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
