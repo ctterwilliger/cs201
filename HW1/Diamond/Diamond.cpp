e5a1134a3437db724eb312b8c23dddabd33593d5
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-01-28 19:20:43 -0900

    Diamond WIP

--- HW1/Diamond/Diamond.cpp
+++ HW1/Diamond/Diamond.cpp
@@ -10,6 +10,38 @@ using namespace std;
 using std::cin;
 int main()
 {
+	int loop = 0;
+	cout << "Please Enter a Positive Integer\n";
+	cin >> loop;
+	while (loop < 1)
+	{
+		cout  << "Your Integer must be positive\n"; 
+		cin >> loop;
+	}
+	for (int i= 0; i < loop; i++)
+	{
+		for (int k = 0; k < loop - i; k++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i + 1; j++)
+		{
+			cout << "#";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < loop; i++)
+	{
+		for (int k = 0; k < i + 2; k++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * (loop - i - 1) - 1; j++)
+		{
+			cout << "#";
+		}
+		cout << "\n";
+	}
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
 	return 0;
