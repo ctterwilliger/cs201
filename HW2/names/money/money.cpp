08c84a2eb9cb2f4bb586a48d2e0d1f1ffda996f1
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-02-08 15:09:58 -0900

    money wip

--- HW2/names/money/money.cpp
+++ HW2/names/money/money.cpp
@@ -13,19 +13,45 @@ using std::cin;
 
 int input(std::string text)
 {
-	int i; 
+	int i =  0;
 	cout << text;
 	cin >> i;
 	while (i < 0)
 	{
-		cout "Please enter a positive number: ";
+		cout << "Please enter a positive number: ";
 		cin >> i; 
 	}
+	return i;
  }
 
+void print_coin(std::string text, int i, std::string text_2)
+{
+	cout << text << i << text_2;
+	if (i != 1)
+	{
+		cout <<"s\n"
+	}
+}
+
+void print_coin_alt_plur(std::string text, int i, std::string text_)
+{
+	cout << text << i << text_2;
+	if (i == 1)
+	{
+		cout << "ies\n";
+	}
+	else
+	{
+		cout << "y\n";
+	}
 int main()
 {
-
+	cout << "This program will calculate the total value of all your coins\n";
+	int pennies = input("Please enter the number of pennies you have: ");
+	int nickles = input("Please enter the number of pennies you have: ");
+	int dimes = input("Please enter the number of pennies you have: ");
+	int quarters = input("Please enter the number of pennies you have: ");
+	int 
 	//Exit code
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
