715b2821d8d3440fe7742a73166179bbf1d9a361
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-03-01 16:58:21 -0900

    Major redesign to handle multiple strings instead of single strings, addition of StringToToken from tokenizer.cpp

--- HW4/fifo-lifo/fifo-lifo.cpp
+++ HW4/fifo-lifo/fifo-lifo.cpp
@@ -9,12 +9,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 using namespace std;
 using std::cin;
 
-bool IscontainerEmpty(std::string text)
+bool IscontainerEmpty(std::vector<std::string> text)
 {
-	if (text == "\0")
+	if (text.size() == 0)
 	{
 		return true;
 	}
@@ -25,26 +26,54 @@ void printContainer(std::vector<std::string> contain)
 {
 	for (auto n : contain)
 	{
-		cout << n; 
+		cout << n << " "; 
 	}
 }
-void lifo(std::string text, std::vector<std::string> & pop)
+void lifo(std::vector<std::string> & text, std::vector<std::string> & pop)
 {
+	pop.push_back(text.back());
+	cout << text.back() << " is being popped out" << endl;
+	std::vector<std::string> placeholder; 
+	for (int i = 0; i < text.size() - 1; i++)
+	{
+		placeholder.push_back(text.at(i));
+	}
+	text = placeholder; 
 
-	
 }
 
-
+unsigned StringToToken(const std::string& input, std::vector<std::string>& tokens)
+{
+	//Converts String into vector of tokens
+	std::istringstream iss(input);
+	std::vector<std::string> token(std::istream_iterator<std::string>{iss},
+		std::istream_iterator<std::string>());
+	// Adds Tokens to existing vector of strings
+	for (auto n : token)
+	{
+		tokens.push_back(n);
+	}
+	return token.size();
+}
 
 int main()
 {
 	std::vector<std::string> pop;
 	std::vector<std::string> container;
-	cin >> text; 
+	std::string text; 
+	std::getline(std::cin, text);
+	StringToToken(text, container);
 	pop = {};
-	while (!(IscontainerEmpty(string)))
+	while (!(IscontainerEmpty(container)))
 	{
-		lifo(text, pop);
+		lifo(container, pop);
+		cout << endl << "The container is now: ";
+		printContainer(container); 
+		cout << endl; 
+	}
+	for (auto n : pop)
+	{
+		cout << n << " ";
 	}
 	//Exit code
 	cout << "Press ENTER to quit ";
