26d4a9854ca043a495d0f73ebc83170c7b0c1aa4
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 13:16:19 -0800

    Hangman Test char WIP

--- HW6/Hangman/Hangman.cpp
+++ HW6/Hangman/Hangman.cpp
@@ -7,13 +7,34 @@
 */
 
 #include <iostream>
-#include <algorithmns> 
+#include <algorithm>
 #include <string>
+#include <map>
 using std::cout;
 using std::cin;
+
+void test_char(std::map<char, int>& map, std::string answer, char guess)
+{
+	if (map[guess] == 0 or map[guess] == 1)
+	{
+		cout << "You have already guessed this character" << std::endl; 
+		return;
+	}
+
+	if ()
+	{
+
+	}
+	map.insert(std::pair<char, int>()
+}
+
+
+
+
 int main()
 {
-
+	std::string answer = "Hello";
+	std::map<char, bool> map;
 
 	//Exit code
 	cout << "Press ENTER to quit ";
