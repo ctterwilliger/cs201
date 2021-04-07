44e68f97a4913d329b2539554a8f44f1601b088d
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 12:57:40 -0800

    Hangman.cpp

---
+++ HW6/Hangman/Hangman.cpp
@@ -0,0 +1,22 @@
+/**
+* file.ccp
+* cs201
+* Clifton Terwilliger
+* 04/07/2021
+* name
+*/
+
+#include <iostream>
+#include <algorithmns> 
+#include <string>
+using std::cout;
+using std::cin;
+int main()
+{
+
+
+	//Exit code
+	cout << "Press ENTER to quit ";
+	while (cin.get() != '\n');
+	return 0;
+}
