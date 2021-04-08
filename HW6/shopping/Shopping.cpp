b0ed02354fd898df6539924f60591f7790835f58
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 16:08:21 -0800

    shopping. file

---
+++ HW6/shopping/Shopping.cpp
@@ -0,0 +1,32 @@
+/**
+* file.ccp
+* cs201
+* Clifton Terwilliger
+* 04/07/2021
+* name
+*/
+
+
+
+
+#include <iostream>
+
+
+struct Record {
+	double unitPrice;
+	int units;
+};
+
+
+
+
+
+using std::cout;
+using std::cin;
+int main()
+{
+	//Exit code
+	cout << "Press ENTER to quit ";
+	while (cin.get() != '\n');
+	return 0;
+}
