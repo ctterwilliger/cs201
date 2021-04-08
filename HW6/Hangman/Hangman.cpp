c5eace59d757e0d8c7eb486c04f9650bf650155f
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 16:04:58 -0800

    Hangman.cpp working

--- HW6/Hangman/Hangman.cpp
+++ HW6/Hangman/Hangman.cpp
@@ -10,15 +10,18 @@
 #include <algorithm>
 #include <string>
 #include <map>
+#include <vector>
+#include <ctype.h>
+#include <stdio.h>
 using std::cout;
 using std::cin;
 
-void test_char(std::map<char, int>& map, std::string answer, char guess)
+bool test_char(std::map<char, int>& map, std::string answer, char guess)
 {
 	if (map[guess] == 0 or map[guess] == 1)
 	{
 		cout << "You have already guessed this character" << std::endl; 
-		return;
+		return true;
 	}
 
 	std::for_each(answer.begin(), answer.end(), [&](char a) {
@@ -30,20 +33,22 @@ void test_char(std::map<char, int>& map, std::string answer, char guess)
 	if (map.at(guess) == 1)
 	{
 		cout << "Your guess is correct" << std::endl; 
-		return;
+		return true;
 	}
 	else
 	{
 		map.at(guess) = 0; 
 		cout << "Your guess is incorrect" << std::endl; 
+		return false;
 	}
 }
 
 
-void print_game_state(std::map<char, int>& m, std::string answer)
+bool print_game_state(std::map<char, int>& m, std::string answer)
 {
 	const std::map<char, int> mm = m; 
 	cout << "Your correct guesses are: ";
+	bool place_holder = true;
 	std::for_each(answer.begin(), answer.end(), [&](char a) mutable {
 		if (mm.at(a) == 1)
 		{
@@ -52,6 +57,7 @@ void print_game_state(std::map<char, int>& m, std::string answer)
 		else
 		{
 			cout << "_ ";
+			place_holder = false;
 		}
 		});
 	cout << "\nIncorrect guesses are: ";
@@ -62,6 +68,7 @@ void print_game_state(std::map<char, int>& m, std::string answer)
 		}
 	});
 	cout << std::endl; 
+	return place_holder;
 }
 
 
@@ -75,10 +82,41 @@ int main()
 			map.emplace(a, 2);
 		}
 	);
-
+	std::vector<std::string> body { "left foot", "right foot", "left hand", " right hand", "left leg", "right leg", "left arm", "right arm", "body", "head" };
+	cout << "You are playing hangman, you have 10 guesses or you die" << std::endl;
 	print_game_state(map, answer);
-	test_char(map, answer, 'a');
-	test_char(map, answer, 'e');
+	int count = 0;
+	while (count < 10)
+	{
+		cout << "Enter a letter" << std::endl;
+		char guess;
+		cin >> guess; 
+		if (std::isalpha(guess))
+		{
+			guess = std::tolower(guess);
+			if (!test_char(map, answer, guess))
+			{
+				cout << "You lost your " << body.at(count) << std::endl;
+				count++;
+			}
+			if (print_game_state(map, answer))
+			{
+				cout <<"You win\n";
+				break;
+			}
+			
+		}
+		else
+		{
+			cout << "You guess was not a letter" << std::endl;
+		}
+		
+	}
+
+	if (count == 10)
+	{
+		cout << "You are dead, you lose\n" << "The word was: " << answer << std::endl;
+	}
 	//Exit code
 	cout << "Press ENTER to quit ";
 	while (cin.get() != '\n');
