e97266cff4f59cfcdf9dcdbcf30e77d0d607cc8d
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 19:46:55 -0800

    Hangman final

--- HW6/Hangman/Hangman.cpp
+++ HW6/Hangman/Hangman.cpp
@@ -1,9 +1,9 @@
 /**
-* file.ccp
+*Hangman.ccp
 * cs201
 * Clifton Terwilliger
 * 04/07/2021
-* name
+* Its hangman the game
 */
 
 #include <iostream>
@@ -16,20 +16,24 @@
 using std::cout;
 using std::cin;
 
+//checks if a char is in the answer
 bool test_char(std::map<char, int>& map, std::string answer, char guess)
 {
+	//checks if it been guessed before and breaking if it has
 	if (map[guess] == 0 or map[guess] == 1)
 	{
 		cout << "You have already guessed this character" << std::endl; 
 		return true;
 	}
 
+	//checks if it is in the answer and setting the value of the key to one
 	std::for_each(answer.begin(), answer.end(), [&](char a) {
 		if (guess == a)
 		{
 			map.at(a) = 1;
 		}
 		});
+	//checks if key is one, if so the user was correct
 	if (map.at(guess) == 1)
 	{
 		cout << "Your guess is correct" << std::endl; 
@@ -43,12 +47,13 @@ bool test_char(std::map<char, int>& map, std::string answer, char guess)
 	}
 }
 
-
+//prints the games state
 bool print_game_state(std::map<char, int>& m, std::string answer)
 {
 	const std::map<char, int> mm = m; 
 	cout << "Your correct guesses are: ";
 	bool place_holder = true;
+	//prints what letters in the answer have been solved, by seeing if the key is 1
 	std::for_each(answer.begin(), answer.end(), [&](char a) mutable {
 		if (mm.at(a) == 1)
 		{
@@ -60,6 +65,7 @@ bool print_game_state(std::map<char, int>& m, std::string answer)
 			place_holder = false;
 		}
 		});
+	//prints incorrect guess by printing all the keys set to  the non defualt 0
 	cout << "\nIncorrect guesses are: ";
 	std::for_each(mm.begin(), mm.end(), [&](std::pair<char, int> pair) {
 		if (pair.second == 0)
@@ -75,6 +81,7 @@ bool print_game_state(std::map<char, int>& m, std::string answer)
 int main()
 {
 	std::string answer = "hello";
+	//Makes a map of aphabet keys and int elements, 2 by default, and 0 if user guessed and is wrong, and 1 if user guessed and is right
 	std::map<char, int> map;
 	std::string alphabet_construct = "abcdefghijklmnopqrstuvwxyz";
 	std::for_each(alphabet_construct.begin(), alphabet_construct.end(), [&](char a) mutable
@@ -82,15 +89,18 @@ int main()
 			map.emplace(a, 2);
 		}
 	);
+	// A vector to iterate through for when the user guesses incorrectly
 	std::vector<std::string> body { "left foot", "right foot", "left hand", " right hand", "left leg", "right leg", "left arm", "right arm", "body", "head" };
 	cout << "You are playing hangman, you have 10 guesses or you die" << std::endl;
 	print_game_state(map, answer);
 	int count = 0;
+	//runs until user produces 10 incorrect guesses
 	while (count < 10)
 	{
 		cout << "Enter a letter" << std::endl;
 		char guess;
 		cin >> guess; 
+		//checks user input, and then calls function
 		if (std::isalpha(guess))
 		{
 			guess = std::tolower(guess);
