9f53db2d8ebba6b0178fb215b0020a1f9b7317aa
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-07 20:13:23 -0800

    Shopping final

--- HW6/shopping/Shopping.cpp
+++ HW6/shopping/Shopping.cpp
@@ -1,9 +1,9 @@
 /**
-* file.ccp
+* Shopping.ccp
 * cs201
 * Clifton Terwilliger
 * 04/07/2021
-* name
+* Rudementary shopping system
 */
 
 
@@ -17,16 +17,23 @@
 #include <numeric>
 using std::cout;
 using std::cin;
+//Creates items to sell
 struct Record {
 	double unitPrice = 0;
 	int units = 0;
 }bread, toast, garlic_bread, biscuit, dough;
 
+//takes map and then requests user for item to add to, then amount and set the key of that value to that amount
 void insert_item(std::map<std::string, Record> & store, std::string input)
 {
 	std::string item; 
 	cout << "Pleae enter the item your would like to add: "; 
 	cin >> item;
+	//checks if it belongs to the map
+	for (auto& n : item)
+	{
+		n = tolower(n);
+	}
 	if (!(item == "bread" || item == "toast" || item == "garlic bread" || item == "bicuit" || item == "dough"))
 	{
 		cout << "Your item was not on the list" << std::endl; 
@@ -40,14 +47,20 @@ void insert_item(std::map<std::string, Record> & store, std::string input)
 		cout << "Your input was invalid please try again: ";
 		cin >> num;
 	}
+	//sets element at key to num + current value
 	store.at(item).units += num;
 }
 
+//sets element to 0 of key
 void remove_item(std::map<std::string, Record>& store, std::string input)
 {
 	std::string item;
 	cout << "Pleae enter the item your would like to remove: ";
 	cin >> item;
+	for (auto& n : item)
+	{
+		n = tolower(n);
+	}
 	if (!(item == "bread" || item == "toast" || item == "garlic bread" || item == "bicuit" || item == "dough"))
 	{
 		cout << "Your item was not on the list" << std::endl;
@@ -55,6 +68,8 @@ void remove_item(std::map<std::string, Record>& store, std::string input)
 	}
 	store.at(item).units = 0;
 }
+
+//prints through items and there amount
 void print_cart( std::map<std::string, Record>& store)
 {
 	cout << "Items in cart are: " << std::endl;
@@ -65,6 +80,7 @@ void print_cart( std::map<std::string, Record>& store)
 int main()
 
 {
+	//creates varibles, map and values of items
 	std::map<std::string, Record> store;
 	toast.unitPrice = 1;
 	bread.unitPrice = 1.1;
@@ -77,17 +93,19 @@ int main()
 	store.emplace("biscuit", biscuit);
 	store.emplace("dough", dough);
 	std::string user_input;
-	print_cart(store);
 
+	//runs until users inputs checkout
 	while (user_input != "checkout")
 	{
 		print_cart(store);
 		cout << "Please enter a command, \"add\" , \"remove\" , and \"checkout\" " << std::endl;
 		cin >> user_input;
+		//sets all elemets to lower case for logic purposes
 		for (auto& n : user_input)
 		{
 			n = tolower(n);
 		}
+		//checks what user command
 		if (user_input.substr(0, 3) == "add")
 		{
 			insert_item(store, user_input);
@@ -108,6 +126,7 @@ int main()
 	{
 		total += n.second.units * n.second.unitPrice; 
 	}
+	cout << total << std::endl; 
 	//I could not get the accumulate function to work, after about an hour and half of trying to figure it out, below is my best attempt. 
 	/*
 	int total = std::accumulate(store.begin(), store.end(), 0, [&](std::pair<std::string, Record> pair) {return pair.second.units * pair.second.unitPrice;});
