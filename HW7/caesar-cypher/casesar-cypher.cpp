c68b55400a531893db5ef6e0053c8d38e3d4ebba
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-19 14:01:39 -0800

    caseasar cypher, cypher char working

--- HW7/caesar-cypher/casesar-cypher.cpp
+++ HW7/caesar-cypher/casesar-cypher.cpp
@@ -10,13 +10,24 @@
 #include <string>
 using std::cout;
 using std::cin;
+void cypher_char(char & c, int cypher)
+{
+	if (islower(c))
+	{
+		c = ((c - 97) + cypher) % 26 + 97;
+	}
+	else if (isupper(c))
+	{
+		c = ((c - 65) + cypher) % 26 + 65;
+	}
+}
 int main()
 {
-	std::string text = "hello";
-	int cypher = 2;
+	std::string text = "Hello, everyone!";
+	int cypher = 3;
 	for (auto& n : text)
 	{
-		n += cypher;
+		cypher_char(n, cypher);
 	}
 	cout << text; 
 	//Exit code
