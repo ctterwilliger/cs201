3145895b647fb2c25a11dc2d90ba2e04f0ca794a
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-19 17:12:16 -0800

    casesar with text loop and error checking

--- HW7/caesar-cypher/casesar-cypher.cpp
+++ HW7/caesar-cypher/casesar-cypher.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 using std::cout;
 using std::cin;
 void cypher_char(char & c, int cypher)
@@ -23,12 +24,32 @@ void cypher_char(char & c, int cypher)
 }
 int main()
 {
-	std::string text = "Hello, everyone!";
-	int cypher = 3;
-	for (auto& n : text)
+	cout << "This program cyphers what you input." << std::endl << "It will do so until you type \"end\", \"END\", or \"End\"" << std::endl; 
+	std::string text;
+	int cypher = 0;
+	std::istringstream input; 
+	do
 	{
-		cypher_char(n, cypher);
-	}
+		for (auto& n : text)
+		{
+			cypher_char(n, cypher);
+		}
+		cout << text << std::endl;
+		cout << "Please enter the text\n" << std::endl;
+		text = ""; 
+		std::getline(cin, text);
+		
+		cout << "Please choose a positive integer you would like to shift by: "; 
+		std::string placeholder;
+		std::getline(cin, placeholder);
+		cypher = stoi(placeholder);
+		while (cypher < 0)
+		{
+			cout << "Your integer was not positive " << std::endl << "Please enter a positive integer: " << std::endl;
+			std::getline(cin, placeholder);
+			cypher = stoi(placeholder);
+		}
+	} while (!(text == "End" || text == "END" || text == "end"));
 	cout << text; 
 	//Exit code
 	cout << "Press ENTER to quit ";
