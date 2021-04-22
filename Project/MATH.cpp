d5797c6357360f63f2242b4fde2fc043fa19375c
Clifton Terwilliger <ctterwilliger@alaska.edu>
2021-04-21 21:44:13 -0800

    add_vectors, handles positive numbers

--- Project/MATH.cpp
+++ Project/MATH.cpp
@@ -19,11 +19,11 @@ void vector_create(int i, std::vector<int>& num)
 	num.resize(0);
 	if (i < 0)
 	{
-		num.push_back(1);
+		num.push_back(-1);
 	}
 	else
 	{
-		num.push_back(0);
+		num.push_back(1);
 	}
 	i = std::abs(i);
 	while (i != 0)
@@ -34,7 +34,7 @@ void vector_create(int i, std::vector<int>& num)
 }
 void print_vector(const std::vector<int>& num)
 {
-	if (num.at(0) == 1)
+	if (num.at(0) == -1)
 	{
 		cout << "-";
 	}
@@ -44,24 +44,52 @@ void print_vector(const std::vector<int>& num)
 	}
 }
 
-void add_vecctors(const std::vector<int>& num1, const std::vector<int>& num2, std::vector<int>& result)
+void add_vectors(const std::vector<int>& num1, const std::vector<int>& num2, std::vector<int>& result)
 {
 	result.resize(0);
+	result.push_back(1);
 	size_t min_size = std::min(num1.size(), num2.size());
 	int place_holder = 0;
-	for (int i = 0; i < min_size; i++)
+	for (int i = 1; i < min_size; i++)
 	{
 		result.push_back((num1.at(i) + num2.at(i) + place_holder) % digit_s);
-		place_holder = num1.at(i) + num2.at(i) + place_holder;
-
+		place_holder = (num1.at(i) + num2.at(i) + place_holder)/digit_s;
 	}
+	if (num1.size() < num2.size())
+	{
+		size_t size = num2.size();
+		for (int i = min_size; i < size; i++)
+		{
+			result.push_back((num2.at(i) + place_holder) % digit_s);
+			place_holder = ( + num2.at(i) + place_holder) / digit_s;
+		}
+	}
+	else if (num1.size() > num2.size())
+	{
+		size_t size = num1.size();
+		for (int i = min_size; i < size; i++)
+		{
+			result.push_back((num1.at(i) + place_holder) % digit_s);
+			place_holder = (num1.at(i)  + place_holder) / digit_s;
+		}
+	}
+	else if(place_holder != 0)
+	{
+		result.push_back(place_holder);
+	}
+
 }
 int main()
 {
 	vector<int> num;
-	int i = -123456789;
+	vector<int> num2;
+	vector<int> result;
+	int i = 123456789;
+	int j = 987654321;
 	vector_create(i, num);
-	print_vector(num);
+	vector_create(j, num2);
+	add_vectors(num, num2, result);
+	print_vector(result);
 	cout << std::endl; 
 
 	//Exit code
